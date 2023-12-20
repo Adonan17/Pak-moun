bd716644e2bd8232fccfb634e4f34faa1121adc9
Adonan17 <Adnaneouassari@gmail.com>
2023-12-20 13:53:27 +0100

    finishing first condition of game loop

--- public/js/main.js
+++ public/js/main.js
@@ -1,74 +1,84 @@
-// creating both pakémoun 
-let paquetchou = {
-    name: 'paquetchou',
-    maxHp: 100,
-    hp: 100,
-    attack: 20,
-    speed: 9
-}
+// random function
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
-let carapills = {
-    name: 'carapills',
-    maxHp: 100,
-    hp: 100,
-    attack: 20,
-    speed: 7
-}
+// game function
 
-// creating the different attacks
+function game() {
 
-function fatalBazookaThunder() {
-    carapills.hp -= (2 * paquetchou.attack);
-}
+    // creating both pakémoun 
 
-function dontMove() {
-    // 50% chance of hitting
-    let proba = getRandomInt(2)
-
-    if (proba == 0) {}
-}
-
-function vomitGun() {
-    // 1% chance of hitting
-    let proba = getRandomInt(100)
-
-    if (proba == 0) {
-        paquetchou.hp -= 100;
-    } else {
-        paquetchou.hp -= carapills.attack;
+    let paquetchou = {
+        name: 'paquetchou',
+        maxHp: 100,
+        hp: 100,
+        attack: 20,
+        speed: 9
     }
-}
 
-function myShellIsSoft() {
-    carapills.hp += 10;
-}
+    let carapills = {
+        name: 'carapills',
+        maxHp: 100,
+        hp: 100,
+        attack: 20,
+        speed: 7
+    }
 
-// creating the game loop
-
-while (carapills.hp > 0 && paquetchou.hp > 0) {
     let turn = 1
 
-    if (turn%2 == 0) {
-        getComputerChoice()
-    } else {
-        
+    // creating the game loop
+
+    while (carapills.hp > 0 && paquetchou.hp > 0) {
+        if (turn%2 == 0) {
+            let proba = getRandomInt(2)
+
+            if (proba == 0) {
+                // 1% chance of hitting
+                let proba = getRandomInt(100)
+
+                if (proba == 0) {
+                    paquetchou.hp -= 100;
+                    alert("unbelievable! carapills just hit paquetchou with a critical 'Vomit Gun' which beats paquetchou in a single shot!!!");
+                    turn += 1;
+                } else {
+                    paquetchou.hp -= carapills.attack;
+                    alert("carapills just used 'Vomit Gun' paquetchou's health drops to " + paquetchou.hp + "HP!");
+                    turn += 1;
+                }
+            } else {
+                    // cannot heal over max HP
+                    if (carapills.hp > 90){
+                        carapills.hp += (carapills.maxHp - carapills.hp);
+                        alert("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. He now has " + carapills.hp + "HP!");
+                        turn += 1;
+                    } else {
+                        carapills.hp += 10;
+                        alert("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. He now has " + carapills.hp + "HP!");
+                        turn += 1;
+                    }
+                };
+        } else {
+            let choice = +prompt('what are you gonna do?')
+
+            if (choice == 0) {
+                carapills.hp -= (2 * paquetchou.attack);
+                alert("this attack was really effective! carapills' health drops to " + carapills.hp + "HP!");
+                turn += 1;
+            } else if (choice == 1) {
+                // 50% chance of hitting
+                let proba = getRandomInt(2)
+
+                if (proba == 0) {
+                    alert("oh, pakéchou used 'do not move!' which means carapills needs to wait another turn before attacking!");
+                    turn += 2;
+                } else {
+                    alert("pakéchou failed to use 'do not move!', nothing changes.")
+                    turn += 1;
+                }
+            }
+        }
     }
 }
 
-// getting the computer choice
-
-function getComputerChoice() {
-    let proba = getRandomInt(2)
-
-    if (proba == 0) {
-        vomitGun()
-    } else {
-        myShellIsSoft()
-    }
-}
-
-// getting the player choice 
-
-function getPlayerChoice() {
-
-}
+game()

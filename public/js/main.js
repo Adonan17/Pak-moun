e493be1c18c10c954962d66efbf7bc3a7a3106a3
Adonan17 <adnaneouassari@gmail.com>
2023-12-22 10:15:15 +0100

    creating v2

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,28 @@ let fatalBazookaThunderBtn = document.querySelector('#fatalBazookaThunderBtn')
 let doNotMoveBtn = document.querySelector('#doNotMoveBtn')
 let textBox = document.querySelector('#textBox')
 let upperScreen = document.querySelector('#bigContenerTop')
-let messageIndex = 0
+let nextBtn = document.querySelector('#next')
+let gameMenuLayout0 = document.querySelector('#gameMenuBtn0')
+let gameMenuLayout1 = document.querySelector('#gameMenuBtn1')
+let gameMenuLayout2 = document.querySelector('#gameMenuBtn2')
+let okBtn = document.querySelector('#ok')
+
+// creating both pakémoun
+let paquetchou = {
+    name: 'paquetchou',
+    maxHp: 100,
+    hp: 90,
+    attack: 15,
+    speed: 9
+}
+
+let carapills = {
+    name: 'carapills',
+    maxHp: 100,
+    hp: 100,
+    attack: 20,
+    speed: 7
+}
 
 // start menu function
 function start() {
@@ -34,121 +55,135 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
-// textbox function
-
-textBox.addEventListener('click', function () {
-    const messages = generateMessages();
-
-    if (messageIndex < messages.length) {
-        textBox.innerHTML = messages[messageIndex];
-        messageIndex++;
-    }
-});
-
-// game function
+//game menu function
 function game() {
     gameMenu.style.display = 'flex';
     mainMenu.style.display = 'none';
     replayMenu.style.display = 'none';
-    upperScreen.style.display = 'flex';
-
-    // creating both pakémoun
-    let paquetchou = {
-        name: 'paquetchou',
-        maxHp: 100,
-        hp: 90,
-        attack: 15,
-        speed: 9
-    }
-
-    let carapills = {
-        name: 'carapills',
-        maxHp: 100,
-        hp: 100,
-        attack: 20,
-        speed: 7
-    }
-
-    // this variable will make sure we have a turn-based system
-    let turn = 1
-    let messages = []
-
-    // creating the game loop
-    while (carapills.hp > 0 && paquetchou.hp > 0) {
-        // this condition will make carapills take his turn
-        if (turn%2 == 0) {
-            fatalBazookaThunderBtn.style.display = 'none'
-            // this will decide which attack carapills will use
-            let proba = getRandomInt(2)
-            // in this case carapills will use 'Vomit Gun'
-            if (proba == 0) {
-                // 1% chance of hitting
-                let proba = getRandomInt(100)
-                // 1/100 chance of one-shotting his opponent
-                if (proba == 0) {
-                    paquetchou.hp -= 100;
-                    messages.push("unbelievable! carapills just hit paquetchou with a critical 'Vomit Gun' which beats paquetchou in a single shot!!!");
-                    turn += 1;
-                // otherwise it will just be a normal attack
-                } else {
-                    paquetchou.hp -= carapills.attack;
-
-                    if (paquetchou.hp > 0) {
-                        messages.push("carapills just used 'Vomit Gun' paquetchou's health drops to " + paquetchou.hp + "HP!");
-                    } else {
-                        messages.push("this attack was fatal, carapills just beat paquetchou!");
-                    }
-                    turn += 1;
-                }
-            } else {
-                    // cannot heal over max HP
-                    if (carapills.hp > 90){
-                        carapills.hp += (carapills.maxHp - carapills.hp);
-                        messages.push("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. He now has " + carapills.hp + "HP!");
-                        turn += 1;
-                    } else if (carapills.hp == 100) {
-                        messages.push("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. But having already all of his health, it wasn't really effective!");
-                        turn += 1;
-                    } else {
-                        carapills.hp += 10;
-                        messages.push("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. He now has " + carapills.hp + "HP!");
-                        turn += 1;
-                    }
-                };
-        } else {
-
-            if (choice == 0) {
-                carapills.hp -= (2 * paquetchou.attack);
-                if (carapills.hp > 0) {
-                    messages.push("this attack was really effective! carapills' health drops to " + carapills.hp + "HP!");
-                } else {
-                    messages.push("this attack was fatal, paquetchou just beat carapills!");
-                }
-                turn += 1;
-            } else if (choice == 1) {
-                // 50% chance of hitting
-                let proba = getRandomInt(2)
-
-                if (proba == 0) {
-                    messages.push("oh, paquetchou used 'do not move!' which means carapills needs to wait another turn before attacking!");
-                    turn += 2;
-                } else {
-                    messages.push("paquetchou failed to use 'do not move!', nothing changes.");
-                    turn += 1;
-                }
-            }
-        }
-    }
-
-    if (carapills.hp <= 0 || paquetchou.hp <= 0) {
-        replay();
-    }
-
-    textBox.addEventListener('click', function () {
-    
-        if (messageIndex < messages.length) {
-            textBox.innerHTML = messages[messageIndex];
-            messageIndex++;
-        }
-    });
 }
+
+//fatal bazooka thunder function
+fatalBazookaThunderBtn.addEventListener('click',)
+
+//do not move function
+doNotMoveBtn.addEventListener('click')
+
+//carapills attack function
+nextBtn.addEventListener('click')
+
+//go back to your turn function
+okBtn.addEventListener('click')
+
+//creating round play function
+function play(a) {
+
+}
+
+// game function
+// function game() {
+//     gameMenu.style.display = 'flex';
+//     mainMenu.style.display = 'none';
+//     replayMenu.style.display = 'none';
+//     upperScreen.style.display = 'flex';
+
+    // // creating both pakémoun
+    // let paquetchou = {
+    //     name: 'paquetchou',
+    //     maxHp: 100,
+    //     hp: 90,
+    //     attack: 15,
+    //     speed: 9
+    // }
+
+    // let carapills = {
+    //     name: 'carapills',
+    //     maxHp: 100,
+    //     hp: 100,
+    //     attack: 20,
+    //     speed: 7
+    // }
+
+//     // this variable will make sure we have a turn-based system
+//     let turn = 1
+//     let messages = []
+
+//     // creating the game loop
+//     while (carapills.hp > 0 && paquetchou.hp > 0) {
+//         // this condition will make carapills take his turn
+//         if (turn%2 == 0) {
+//             fatalBazookaThunderBtn.style.display = 'none'
+//             // this will decide which attack carapills will use
+//             let proba = getRandomInt(2)
+//             // in this case carapills will use 'Vomit Gun'
+//             if (proba == 0) {
+//                 // 1% chance of hitting
+//                 let proba = getRandomInt(100)
+//                 // 1/100 chance of one-shotting his opponent
+//                 if (proba == 0) {
+//                     paquetchou.hp -= 100;
+//                     messages.push("unbelievable! carapills just hit paquetchou with a critical 'Vomit Gun' which beats paquetchou in a single shot!!!");
+//                     turn += 1;
+//                 // otherwise it will just be a normal attack
+//                 } else {
+//                     paquetchou.hp -= carapills.attack;
+
+//                     if (paquetchou.hp > 0) {
+//                         messages.push("carapills just used 'Vomit Gun' paquetchou's health drops to " + paquetchou.hp + "HP!");
+//                     } else {
+//                         messages.push("this attack was fatal, carapills just beat paquetchou!");
+//                     }
+//                     turn += 1;
+//                 }
+//             } else {
+//                     // cannot heal over max HP
+//                     if (carapills.hp > 90){
+//                         carapills.hp += (carapills.maxHp - carapills.hp);
+//                         messages.push("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. He now has " + carapills.hp + "HP!");
+//                         turn += 1;
+//                     } else if (carapills.hp == 100) {
+//                         messages.push("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. But having already all of his health, it wasn't really effective!");
+//                         turn += 1;
+//                     } else {
+//                         carapills.hp += 10;
+//                         messages.push("carapills used 'My Shell Is Soft' which grants him 10% of his max health points. He now has " + carapills.hp + "HP!");
+//                         turn += 1;
+//                     }
+//                 };
+//         } else {
+
+//             if (choice == 0) {
+//                 carapills.hp -= (2 * paquetchou.attack);
+//                 if (carapills.hp > 0) {
+//                     messages.push("this attack was really effective! carapills' health drops to " + carapills.hp + "HP!");
+//                 } else {
+//                     messages.push("this attack was fatal, paquetchou just beat carapills!");
+//                 }
+//                 turn += 1;
+//             } else if (choice == 1) {
+//                 // 50% chance of hitting
+//                 let proba = getRandomInt(2)
+
+//                 if (proba == 0) {
+//                     messages.push("oh, paquetchou used 'do not move!' which means carapills needs to wait another turn before attacking!");
+//                     turn += 2;
+//                 } else {
+//                     messages.push("paquetchou failed to use 'do not move!', nothing changes.");
+//                     turn += 1;
+//                 }
+//             }
+//         }
+//     }
+
+//     if (carapills.hp <= 0 || paquetchou.hp <= 0) {
+//         replay();
+//     }
+
+//     textBox.addEventListener('click', function () {
+    
+//         if (messageIndex < messages.length) {
+//             textBox.innerHTML = messages[messageIndex];
+//             messageIndex++;
+//         }
+//     });
+// }
+
